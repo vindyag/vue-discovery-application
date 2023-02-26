86d1dbe4918ca6538959e3a1763ed441052b7eee
Vindya Lakimali Gunawardena <99x-vindyag@driw.no>
2023-02-26 22:55:20 +0530

    add relevant properties

---
+++ gateway-service/src/main/java/gateway/GatewayServiceApplication.java
@@ -0,0 +1,9 @@
+package gateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayServiceApplication {
+}
