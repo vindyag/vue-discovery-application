811d7afdea78380168ef8841f56058ca8beda59c
Vindya Lakimali Gunawardena <99x-vindyag@driw.no>
2023-03-09 12:09:29 +0530

    update gateway service

--- gateway-service/src/main/java/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/gateway/GatewayServiceApplication.java
@@ -1,9 +1,14 @@
 package gateway;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @SpringBootApplication
-public class GatewayServiceApplication {
+public class  GatewayServiceApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
 }
