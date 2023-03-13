a5477f386183792786268ac26bad9b5c588664c0
Vindya Lakimali Gunawardena <99x-vindyag@driw.no>
2023-03-13 11:55:23 +0530

    add back-end core services

---
+++ core-app-service-2/src/main/java/core/app2/Core2Application.java
@@ -0,0 +1,11 @@
+package core.app2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Core2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Core2Application.class, args);
+    }
+}
